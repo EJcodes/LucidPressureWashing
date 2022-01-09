0df70b9e8ded75c72cece19471a5532542931c95
Evan Londono <evanlondono@Evans-MacBook-Pro.local>
2022-01-08 20:08:27 -0500

    began to add basic Nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 import Home from './booking/Home';
 import Login from './auth/Login';
 import Register from './auth/Register';
 
+
+const TopNav = () => (
+  <div className=" nav bg-light d-flex justify-content-between">
+    <Link className="nav-link" to="/">
+      Home
+    </Link>
+    <Link className="nav-link" to="/register">
+      Register
+    </Link>
+    <Link className="nav-link" to="/login">
+      Login
+    </Link>
+  </div>
+)
+
 function App() {
   return (
     <Router>
+      {TopNav()}
     <Routes>
       <Route exact path="/" element={<Home/>} />
       <Route exact path="/login" element={<Login/>} />
