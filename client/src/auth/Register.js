53520023666bd69328ac2ff5b459f9015585c192
Evan Londono <evanlondono@Evans-MacBook-Pro.local>
2022-01-10 20:28:02 -0500

    connected mongodb database and now working on the form submit functions and UI to

--- client/src/auth/Register.js
+++ client/src/auth/Register.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import {useState} from 'react';
+
+
 
 const Register = () => {
+
+    const [ name, setName ] = useState('');
+    const [ email, setEmail ] = useState('');
+    const [ password, setPassword ] = useState('');
+
+    const  handleSubmit = () =>{
+        alert('send user info to backend')
+    }
+    const registerForm = () =>  <form onSubmit={handleSubmit}>show the form</form>
+    
     return (
-        <div className="container-fluid h1 p-5 text-center"> 
-        Register Here!
+        <>
+        <div className="container-fluid bg-secondary h1 p-5 text-center"> 
+        <h1>Register</h1>
         </div>
+
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6 offset-md-3">
+                    {registerForm()}
+
+                </div>
+            </div>
+        </div>
+        </>
     )
 }
 
