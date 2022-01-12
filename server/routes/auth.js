15e694d05cc3bb671b2729588b2234f6750c5589
Evan Londono <evanlondono@Evans-MacBook-Pro.local>
2022-01-12 11:23:35 -0500

    fixed error in user Schema state was not yourState

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,10 +3,10 @@ import express from 'express';
 const router = express.Router();
 
 //controllers
-import { showMessage, register } from "../controllers/auth";
+import { register } from "../controllers/auth";
 
-router.get("/:message", showMessage);
-router.post('/register', register)
+
+router.post("/register", register);
 
 
 
